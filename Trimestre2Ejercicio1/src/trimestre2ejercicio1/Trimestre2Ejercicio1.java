561fe4b319ba5783828a8d86dafd407c31006908
Nick Morales <nick.morales@cesjuanpablosegundo.es>
2019-01-31 08:17:15 +0100

    segunda version de Ej. java 1 (nota examen) "Esta si bucle"

--- Trimestre2Ejercicio1/src/trimestre2ejercicio1/Trimestre2Ejercicio1.java
+++ Trimestre2Ejercicio1/src/trimestre2ejercicio1/Trimestre2Ejercicio1.java
@@ -5,6 +5,8 @@
  */
 package trimestre2ejercicio1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario DAM 1
@@ -15,7 +17,24 @@ public class Trimestre2Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        double nota1 = 0;
+        double nota2 = 0;
+        double notaf = 0;
+
+        System.out.println("Introduce la nota del primer examen: ");
+        nota1 = entrada.nextDouble();
+
+        System.out.println("¿Qué nota quieres sacra en el primer trimestre?");
+        notaf = entrada.nextDouble();
+        if (nota1 > 0 && nota1 < 11 && notaf > 0 && notaf <= 10) {
+
+            nota2 = ((notaf * 100) - (nota1 * 40)) / 60;
+            System.out.println("Si quieres sacar un " + notaf + "en la nota final, necesitas sacar un " + nota2 + " en la segunda nota del examen");
+        } else {
+            System.out.println("Has introducido una nota invalida");
+        }
+
     }
-    
+
 }
